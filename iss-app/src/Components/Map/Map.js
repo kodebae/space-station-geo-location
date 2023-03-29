0921d7a7912bba033441c93a493b7e9fdfa53e90
kodebae <heykarmendurbin@gmail.com>
2023-03-29 18:30:00 -0400

    chore: added lat and long for NASA Space Center

--- iss-app/src/Components/Map/Map.js
+++ iss-app/src/Components/Map/Map.js
@@ -1,17 +1,21 @@
 import React from 'react'
-import { useEffect, useRef } from 'react'
+import { useEffect } from 'react'
 import * as tt from '@tomtom-international/web-sdk-maps'
 import './Map.css'
 
+const long = -80.651070
+const lat = 28.573469
+
 
 const Map = () => {
-    const mapElement = useRef()
 
 
    useEffect(()=> {
     var map = tt.map({
         key: process.env.REACT_APP_API_KEY,
         container: 'map',
+        center: [long, lat],
+        zoom: 14,
     });
     map.addControl(new tt.FullscreenControl());
     map.addControl(new tt.NavigationControl());
@@ -19,7 +23,7 @@ const Map = () => {
    }, [])
 
   return (
-    <div id="map" ref={mapElement} className='map' style={{width: "100%", height: "100%"}}>
+    <div id="map">
    
     </div>
   )
