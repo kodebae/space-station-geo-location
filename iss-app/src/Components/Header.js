91e7714c095bd076101d0b72f9e71377776919fc
kodebae <heykarmendurbin@gmail.com>
2023-03-30 22:22:13 -0400

    Styles: updates

--- iss-app/src/Components/Header.js
+++ iss-app/src/Components/Header.js
@@ -4,6 +4,7 @@ import { useCallback } from "react"
 import Particles from "react-tsparticles"
 import { loadFull } from "tsparticles"
 
+
 const Header = () => {
 
   const particlesInit = useCallback(async engine => {
--- iss-app/src/Components/Logo/Logo.js
+++ iss-app/src/Components/Logo/Logo.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import Tilt from 'react-parallax-tilt';
-import './Logo.css';
-import image from './ISS_emblem.png';
+import React from 'react'
+import Tilt from 'react-parallax-tilt'
+import './Logo.css'
+import image from './ISS_emblem.png'
 
 const Logo = () => (
   <Tilt tiltReverse={true}>
     <div>
-        <img alt='iss_emblem' src={image}/>
+        <img className="logo" alt='iss_emblem' src={image}/>
     </div>
   </Tilt>
 );
 
-export default Logo;
+export default Logo
