43a390e56cfa390d27b6862b87b3a3641bee1351
kodebae <heykarmendurbin@gmail.com>
2023-03-31 21:30:56 -0400

    feat: icon now on top pf map with zindex pos rel

--- iss-app/src/App.js
+++ iss-app/src/App.js
@@ -2,14 +2,22 @@ import './App.css'
 import Header from './Components/Header'
 import Map from './Components/Map/Map'
 
-
 function App() {
+ 
+
+
   return (
     <div className="App">
       <Header />
-      <Map />
+      <Map /> 
     </div>
   );
 }
 
 export default App;
+
+/**
+ *? The dependency array basically tells the "useEffect" hook to "only trigger when the dependency array changes".  
+ *? Deconstructed elements from the props are being consumed to locate the iss and pinpoint the location on the map. 
+ *? ParseFloat api returns a string so the string must be parsed in order to display the decimal numbers for the longitude and latitude.
+ */
--- iss-app/src/Components/Map/Map.js
+++ iss-app/src/Components/Map/Map.js
@@ -1,36 +1,59 @@
 import React from 'react'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
+// import * as ttapi from '@tomtom-international/web-sdk-services'
 import * as tt from '@tomtom-international/web-sdk-maps'
+import '@tomtom-international/web-sdk-maps/dist/maps.css'
 import './Map.css'
-
-
+import icon from './ISS_spacecraft_model_icon.png'
+import axios from 'axios'
 
 
 
 const Map = () => {
 
-    const [map, setMap] = useState({})
+  const [longitude, setLongitude] = useState(-121.91599);
+  const [latitude, setLatitude] = useState(37.36765);
 
-    const long = -80.651070
-    const lat = 28.573469
+
+  useEffect(() => {
+    getLocation()
+  }, [])
+
+  const getLocation = async () => {
+    const res = await axios.get('http://api.open-notify.org/iss-now.json')
+
+    let longitude = res.data.iss_position
+    let latitude = res.data.iss_position
+
+    setLongitude(parseFloat(longitude))
+    setLatitude(parseFloat(latitude))
+    
+  }
+
+
+    const mapElement = useRef();
+    const [map, setMap] = useState({})
 
    useEffect(()=> {
     var map = tt.map({
         key: process.env.REACT_APP_API_KEY,
         container: 'map',
-        center: [long, lat],
-        zoom: 14,
+        center: [longitude, latitude],
+        zoom: 8,
     });
+
     map.addControl(new tt.FullscreenControl())
     map.addControl(new tt.NavigationControl())
 
     setMap(map)
-
    }, [])
 
+ 
+
   return (
+
     <div id="map" className='map-container'>
-   
+      <img src={icon} className='iss-icon' alt='iss-icon'/>
     </div>
   )
 }
@@ -38,5 +61,8 @@ const Map = () => {
 export default Map
 
 /**
- *? The dependency array basically tells the "useEffect" hook to "only trigger when the dependency array changes".  
+ *? The dependency array basically tells the "useEffect" React hook to "only trigger when the dependency array changes".  
  * **/
+
+
+   // const { longitude, latitude } = await res.data.iss_position
