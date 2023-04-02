e38540edacad85eeedb3050ce79be34a316e8616
kodebae <heykarmendurbin@gmail.com>
2023-04-02 19:22:00 -0400

    docs: update

--- iss-app/src/Components/Map/Map.js
+++ iss-app/src/Components/Map/Map.js
@@ -1,74 +1,74 @@
-import React, { useState, useEffect } from 'react';
-import * as tt from '@tomtom-international/web-sdk-maps';
-import '@tomtom-international/web-sdk-maps/dist/maps.css';
-import './Map.css';
-import axios from 'axios';
+import React, { useState, useEffect } from 'react'
+import * as tt from '@tomtom-international/web-sdk-maps'
+import '@tomtom-international/web-sdk-maps/dist/maps.css'
+import './Map.css'
+import axios from 'axios'
 
 const Map = () => {
-  const [map, setMap] = useState(null);
-  const [mapLongitude, setMapLongitude] = useState(-74.0059);
-  const [mapLatitude, setMapLatitude] = useState(40.7128);
+  const [map, setMap] = useState(null)
+  const [mapLongitude, setMapLongitude] = useState(-74.0059)
+  const [mapLatitude, setMapLatitude] = useState(40.7128)
 
   const getLocation = async () => {
     try {
-      const res = await axios.get('http://api.open-notify.org/iss-now.json');
+      const res = await axios.get('http://api.open-notify.org/iss-now.json')
   
-      let longitude = res.data.iss_position.longitude;
-      let latitude = res.data.iss_position.latitude;
+      let longitude = res.data.iss_position.longitude
+      let latitude = res.data.iss_position.latitude
   
-      setMapLongitude(parseFloat(longitude));
-      setMapLatitude(parseFloat(latitude));
+      setMapLongitude(parseFloat(longitude))
+      setMapLatitude(parseFloat(latitude))
 
       // Initialize a new map instance with the updated coordinates
       const newMap = tt.map({
         key: process.env.REACT_APP_API_KEY,
         container: 'map',
         center: [longitude, latitude],
-        zoom: 8,
-      });
+        zoom: 3,
+      })
 
       // Remove the old map instance
       if (map) {
-        map.remove();
+        map.remove()
       }
 
       // Set the new map instance
-      setMap(newMap);
+      setMap(newMap)
     } catch (error) {
-      console.log('Error getting ISS position:', error);
+      console.log('Error getting ISS position:', error)
     }
-  };
+  }
 
   useEffect(() => {
-    console.log('useEffect called!');
+    console.log('useEffect called!')
     const intervalId = setInterval(() => {
-      getLocation();
-    }, 5000);
+      getLocation()
+    }, 5000)
   
-    return () => clearInterval(intervalId);
-  }, []);
+    return () => clearInterval(intervalId)
+  }, [])
   
 
   useEffect(() => {
     if (map) {
-      const markerElement = document.createElement('div');
+      const markerElement = document.createElement('div')
       markerElement.className = 'marker';
 
       const marker = new tt.Marker({
         element: markerElement,
       })
         .setLngLat([mapLongitude, mapLatitude])
-        .addTo(map);
+        .addTo(map)
 
-      map.addControl(new tt.FullscreenControl());
-      map.addControl(new tt.NavigationControl());
+      map.addControl(new tt.FullscreenControl())
+      map.addControl(new tt.NavigationControl())
     }
-  }, [map, mapLongitude, mapLatitude]);
+  }, [map, mapLongitude, mapLatitude])
 
-  return <div id="map" className="map-container" />;
-};
+  return <div id="map" className="map-container" />
+}
 
-export default Map;
+export default Map
 
 
 
